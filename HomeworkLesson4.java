4d1a2cd31efa773f5c1d29072a20d372b9d712dc
Сергей Артёмов <35760591+SergeyArtsiomau@users.noreply.github.com>
2018-06-06 10:54:45 +0300

    HomeWorkLesson4

---
+++ HomeworkLesson4.java
@@ -0,0 +1,78 @@
+package com.gmail.xmrayq.main;
+
+public class HomeworkLesson4 {
+
+    public static void main(String[] args) {
+
+        int[] massive1 = new int[4];
+        int[] massive2 = new int[4];
+
+        massive1[0] = 3;
+        massive1[1] = 4;
+        massive1[2] = 2;
+        massive1[3] = 6;
+
+        massive2[0] = 5;
+        massive2[1] = 9;
+        massive2[2] = 7;
+        massive2[3] = 3;
+
+        int[] massive3 = new int[8];
+
+        massive3[0] = massive1[0];
+        massive3[1] = massive1[1];
+        massive3[2] = massive1[2];
+        massive3[3] = massive1[3];
+        massive3[4] = massive2[0];
+        massive3[5] = massive2[1];
+        massive3[6] = massive2[2];
+        massive3[7] = massive2[3];
+
+        System.out.print(massive3[0]);
+        System.out.print(" ");
+        System.out.print(massive3[1]);
+        System.out.print(" ");
+        System.out.print(massive3[2]);
+        System.out.print(" ");
+        System.out.print(massive3[3]);
+        System.out.print(" ");
+        System.out.print(massive3[4]);
+        System.out.print(" ");
+        System.out.print(massive3[5]);
+        System.out.print(" ");
+        System.out.print(massive3[6]);
+        System.out.print(" ");
+        System.out.println(massive3[7]);
+
+
+        int year = 2024;
+
+        if (((year % 4) == 0) || ((year % 400 == 0) && (year % 100 != 0))) {
+            System.out.println("Number days in a year = 366");
+        } else {
+            System.out.println("Number days in a year = 365");
+        }
+
+        int time = 4;
+
+        switch (time) {
+            case 1:
+                System.out.println("Spring");
+                break;
+            case 2:
+                System.out.println("Summer");
+                break;
+            case 3:
+                System.out.println("Autumn");
+                break;
+            case 4:
+                System.out.println("Winter");
+                break;
+            default:
+                System.out.println("There is no such time in the year");
+
+        }
+
+    }
+
+}
