58b1d18f9f70d4ad55225fc8681d47ee27f91a87
Сергей Артёмов <35760591+SergeyArtsiomau@users.noreply.github.com>
2018-06-07 22:18:30 +0300

    HomeworkLesson5

---
+++ com/gmail/xmrayq/main/Integers.java
@@ -0,0 +1,17 @@
+package com.gmail.xmrayq.main1;
+
+public class Integers {
+
+    public void naturalNumbers(int start, int end) {
+        int result;
+        for (int i = start; i <= end; i++) {
+            if (i % 3 == 0) {
+                if (i % 5 != 0) {
+                    System.out.println(i);
+                }
+            }
+
+
+        }
+    }
+}
--- com/gmail/xmrayq/main/Main.java
+++ com/gmail/xmrayq/main/Main.java
@@ -1,19 +1,14 @@
-package com.gmail.xmrayq.main;
+package com.gmail.xmrayq.main1;
 
 public class Main {
     public static void main(String[] args) {
-
-        CreditCard creditCard1 = new CreditCard(347575899, 340);
-        creditCard1.printInfo();
-        creditCard1.addMoney(110);
-
-        CreditCard creditCard2 = new CreditCard(578699905, 270);
-        creditCard2.printInfo();
-        creditCard2.addMoney(230);
-
-        CreditCard creditCard3 = new CreditCard(789030482, 560);
-        creditCard3.printInfo();
-        creditCard3.differenceMoney(120);
-
+        Years days = new Years();
+        SquareOfNumbers numbers = new SquareOfNumbers();
+        Integers integers = new Integers();
+        days.calculateNumbers(2000, 2010);
+        System.out.println("___________________________");
+        numbers.squareNumbers(10, 20);
+        System.out.println("___________________________");
+        integers.naturalNumbers(20, 50);
     }
 }
